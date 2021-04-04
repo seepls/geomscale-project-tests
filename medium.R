cc2b3aec6ba8ec1569fdd4a27eb7ea2eb59729e6
Smriti Tiwari <smrititiwari2016@gmail.com>
2021-04-04 07:19:48 +0530

    Create medium.R

---
+++ medium.R
@@ -0,0 +1,2 @@
+# Sample approximate uniformly points from a random generated polytope using the implmented in volesti random walks, for various walk lengths.
+# For each sample compute the PSRF and report on the results.
