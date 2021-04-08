30d876170ab26e2f4ed67fc4aaa16154ce2caa10
Smriti Tiwari <smrititiwari2016@gmail.com>
2021-04-08 09:13:36 +0530

    Update Gibbs_sampling.hpp

--- Gibbs_sampling.hpp
+++ Gibbs_sampling.hpp
@@ -1,1 +1,24 @@
 
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <armadillo>
+#include <boost/random.hpp>
+#include <boost/math/distributions.hpp>
+
+#include "Sampling_functions.h"
+
+
+using namespace std;
+using namespace Eigen;
+using namespace arma;
+using namespace boost;
+
+
+//boost::random::mt19937 gen(0);
+boost::random::mt19937 gen(time(0));
+//distributions
+double runif(double lower, double higher)
+{
+	boost::random::uniform_real_distribution<> dist(lower, higher);
+	return dist(gen);
+}
